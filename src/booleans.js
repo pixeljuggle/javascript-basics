f85b318c5041e64c9c46f0094860040921da6848
Alex White <pixeljuggle@gmail.com>
2021-03-02 20:25:42 +0000

    Completed booleans

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,66 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b;
 };
 
 const none = (a, b) => {
-  // your code here
+  return !a && !b;
 };
 
 const one = (a, b) => {
-  // your code here
+  return !( ( a && b ) || ( !a && !b ) );
 };
 
 const truthiness = a => {
-  // your code here
+  if (a) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return ( a > b );
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return ( a <= b );
 };
 
 const isOdd = a => {
-  // your code here
+  return ( (a % 2) > 0 );
 };
 
 const isEven = a => {
-  // your code here
+  return ( (a % 2) === 0 );
 };
 
 const isSquare = a => {
-  // your code here
+  return ( Math.sqrt(a) % 1 === 0 );
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return ( char === string.charAt(0) );
 };
 
 const containsVowels = string => {
-  // your code here
+  lowString = string.toLowerCase();
+  return ( lowString.includes("a") || lowString.includes("e") || lowString.includes("i") || lowString.includes("o") || lowString.includes("u") );
 };
 
 const isLowerCase = string => {
-  // your code here
+  return ( string.toLowerCase() === string );
 };
 
 module.exports = {
